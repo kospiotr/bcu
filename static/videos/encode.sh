56def7fe4f0b79381e96721f00cd1ac22356f46a
Piotr Kosmowski <kospiotr@gmail.com>
2024-09-23 10:25:20 +0200

    Update

--- static/videos/encode.sh
+++
@@ -1,4 +0,0 @@
-#ffmpeg -y -i "hero.mp4" -c:a aac -b:a 192k -vn "output_audio.m4a"
-#ffmpeg -y -i "hero.mp4" -preset slow -tune film -vsync passthrough -an -c:v libx264 -x264opts 'keyint=25:min-keyint=25:no-scenecut' -crf 22  -maxrate 5000k -bufsize 10000k -pix_fmt yuv420p -f mp4 "hero_5000.mp4"
-#ffmpeg -y -i "hero.mp4" -preset slow -tune film -vsync passthrough -an -c:v libx264 -x264opts 'keyint=25:min-keyint=25:no-scenecut' -crf 23  -maxrate 2000k -bufsize 4000k -pix_fmt yuv420p -f mp4 "hero_2000.mp4"
-MP4Box -dash 2000 -rap -frag-rap  -bs-switching no -profile "dashavc264:live" "hero_5000.mp4" "hero_2000.mp4"  -out "hero.mpd"
--- themes/theme1/assets/js/index.js
+++ themes/theme1/assets/js/index.js
@@ -2,17 +2,20 @@ import Toast from "js/bootstrap/src/toast";
 import Popover from "js/bootstrap/src/popover";
 
 document.addEventListener("DOMContentLoaded", function(){
+  let navbar_top = document.getElementById('navbar_top');
+  let navbar_height = navbar_top.offsetHeight;
+  let classList = navbar_top.classList;
+  classList.add('p-5');
   window.addEventListener('scroll', function() {
-    let navbar_top = document.getElementById('navbar_top');
-    let navbar_height = navbar_top.offsetHeight;
-    let classList = navbar_top.classList;
     if (window.scrollY <= navbar_top.offsetHeight) {
       classList.remove("shadow");
       classList.remove('bg-primary');
+      classList.add('p-5');
 //      document.body.style.paddingTop = '0';
     } else {
       classList.add("shadow");
       classList.add('bg-primary');
+      classList.remove('p-5');
 //      document.body.style.paddingTop = navbar_height + 'px';
     }
   });
